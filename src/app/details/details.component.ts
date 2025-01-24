c724e01ae8a2f855dd5cf0769605f1b1cee14857
kosti996 <kostiukevycha@gmail.com>
2025-01-24 19:44:57 +0100

    updated details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
@@ -11,9 +13,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getAllHousingLocationById(housingLocationId);
   }
 }
